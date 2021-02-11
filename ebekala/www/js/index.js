5de82549c0b99376e1c521e85253242545c08b74
walaa111 <walaagamil18@gmail.com>
2021-02-11 22:35:48 +0200

    edit

--- ebekala/www/js/index.js
+++ ebekala/www/js/index.js
@@ -36,39 +36,7 @@
 
 var app = {
 
-   //notify//
 
-
-// Add to index.js or the first page that loads with your app.
-
-receivedEvent: function(id) {
-  //START ONESIGNAL CODE
-  //Remove this method to stop OneSignal Debugging 
-  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-  // Set your iOS Settings
-  var iosSettings = {};
-  iosSettings["kOSSettingsKeyAutoPrompt"] = false;
-  iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
-  
-  window.plugins.OneSignal
-    .startInit("7b232030-af95-4654-87af-48cfd36354b3")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .iOSSettings(iosSettings)
-    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
-    .endInit();
-  
-  // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
-  window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
-    console.log("User accepted notifications: " + accepted);
-  });
-  //END ONESIGNAL CODE
-},
-
- //end of notify//
     // Application Constructor
 
     initialize: function () {
@@ -160,9 +128,37 @@ inAppBrowserbRef.addEventListener('loadstart', function() {
 };
 
 
-                //admob
+          //notify//
 
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
 
-           //admob
+document.addEventListener('deviceready', function () {
+  //Remove this method to stop OneSignal Debugging 
+  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+  // Set your iOS Settings
+  var iosSettings = {};
+  iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+  iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+  
+  window.plugins.OneSignal
+    .startInit("7b232030-af95-4654-87af-48cfd36354b3")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .iOSSettings(iosSettings)
+    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+    .endInit();
+  
+  // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
+  window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+    console.log("User accepted notifications: " + accepted);
+  });
+  
+}, false),
+
+ //end of notify//
 
 app.initialize();
