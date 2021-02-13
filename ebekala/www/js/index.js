dc1221c5a70acce566c6be67db93a3bbeee351d6
walaa111 <walaagamil18@gmail.com>
2021-02-13 19:31:47 +0200

    edit

--- ebekala/www/js/index.js
+++ ebekala/www/js/index.js
@@ -101,29 +101,6 @@ inAppBrowserbRef.addEventListener('loadstart', function() {
     },
 
 
-
-    // Update DOM on a Received Event
-
-    receivedEvent: function (id) {
-
-        var parentElement = document.getElementById(id);
-
-        var listeningElement = parentElement.querySelector('.listening');
-
-        var receivedElement = parentElement.querySelector('.received');
-
-
-
-        listeningElement.setAttribute('style', 'display:none;');
-
-        receivedElement.setAttribute('style', 'display:block;');
-
-
-
-        console.log('Received Event: ' + id);
-
-
-    },
           //notify//
      
 // Add to index.js or the first page that loads with your app.
