848c7a999dc112bf2bbe76c6cb3be5421cba0346
walaa111 <walaagamil18@gmail.com>
2021-02-14 20:03:54 +0200

    edit

--- ebekala/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ ebekala/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,39 +1,27 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-connectivity-monitor.connectivity",
-    "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
-    "pluginId": "cordova-connectivity-monitor",
-    "clobbers": [
-      "window.connectivity"
-    ]
-  },
-  {
-    "id": "cordova-admob.AdMobAds",
-    "file": "plugins/cordova-admob/www/admob.js",
-    "pluginId": "cordova-admob",
-    "clobbers": [
-      "window.admob",
-      "window.tappx"
-    ]
-  },
-  {
-    "id": "onesignal-cordova-plugin.OneSignal",
-    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-    "pluginId": "onesignal-cordova-plugin",
-    "clobbers": [
-      "OneSignal"
-    ]
-  }
-];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-  "cordova-connectivity-monitor": "1.2.2",
-  "cordova-play-services-version-adapter": "1.0.2",
-  "cordova-admob": "5.1.0",
-  "cordova-android-firebase-gradle-release": "4.0.0",
-  "onesignal-cordova-plugin": "2.11.3"
-};
-// BOTTOM OF METADATA
+  module.exports = [
+    {
+      "id": "cordova-connectivity-monitor.connectivity",
+      "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
+      "pluginId": "cordova-connectivity-monitor",
+      "clobbers": [
+        "window.connectivity"
+      ]
+    },
+    {
+      "id": "cordova-admob.AdMobAds",
+      "file": "plugins/cordova-admob/www/admob.js",
+      "pluginId": "cordova-admob",
+      "clobbers": [
+        "window.admob",
+        "window.tappx"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-connectivity-monitor": "1.2.2",
+    "cordova-play-services-version-adapter": "1.0.2",
+    "cordova-admob": "5.1.0",
+    "cordova-android-firebase-gradle-release": "4.0.0"
+  };
 });
