c3d4fa2d0bae71cd520518a137f7eadf0dc04713
walaa111 <walaagamil18@gmail.com>
2021-02-14 22:25:12 +0200

    edit

--- ebekala/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ ebekala/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "onesignal-cordova-plugin.OneSignal",
-    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-    "pluginId": "onesignal-cordova-plugin",
-    "clobbers": [
-      "OneSignal"
-    ]
-  },
-  {
     "id": "cordova-plugin-inappbrowser.inappbrowser",
     "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
     "pluginId": "cordova-plugin-inappbrowser",
@@ -39,7 +31,6 @@ module.exports.metadata =
 {
   "cordova-android-firebase-gradle-release": "4.0.0",
   "cordova-android-support-gradle-release": "3.0.1",
-  "onesignal-cordova-plugin": "2.11.3",
   "cordova-plugin-whitelist": "1.3.4",
   "cordova-plugin-inappbrowser": "4.1.0",
   "cordova-connectivity-monitor": "1.2.2",
--- ebekala/www/js/index.js
+++ ebekala/www/js/index.js
@@ -70,31 +70,7 @@ var app = {
 // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 
-document.addEventListener('deviceready', function () {
-  //Remove this method to stop OneSignal Debugging 
-  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-  // Set your iOS Settings
-  var iosSettings = {};
-  iosSettings["kOSSettingsKeyAutoPrompt"] = false;
-  iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
-  
-  window.plugins.OneSignal
-    .startInit("4c4cde84-e951-4d25-849c-1a95bd6d5e26")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .iOSSettings(iosSettings)
-    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
-    .endInit();
-  
-  // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
-  window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
-    console.log("User accepted notifications: " + accepted);
-  });
-  
-}, false);
+
   },
 
   // Update DOM on a Received Event
