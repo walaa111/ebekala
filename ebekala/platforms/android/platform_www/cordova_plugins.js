4f5296cdc2c5e1dc2be367efaac917004180c1bc
walaa111 <walaagamil18@gmail.com>
2021-02-14 20:10:17 +0200

    eddit

--- ebekala/platforms/android/platform_www/cordova_plugins.js
+++ ebekala/platforms/android/platform_www/cordova_plugins.js
@@ -16,12 +16,21 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
         "window.admob",
         "window.tappx"
       ]
+    },
+    {
+      "id": "onesignal-cordova-plugin.OneSignal",
+      "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+      "pluginId": "onesignal-cordova-plugin",
+      "clobbers": [
+        "OneSignal"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-connectivity-monitor": "1.2.2",
     "cordova-play-services-version-adapter": "1.0.2",
     "cordova-admob": "5.1.0",
-    "cordova-android-firebase-gradle-release": "4.0.0"
+    "cordova-android-firebase-gradle-release": "4.0.0",
+    "onesignal-cordova-plugin": "2.11.3"
   };
 });
