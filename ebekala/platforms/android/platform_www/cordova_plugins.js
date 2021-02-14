9b744f1d782830d65937070aa327fe6c8a85d248
walaa111 <walaagamil18@gmail.com>
2021-02-14 21:38:20 +0200

    edit

--- ebekala/platforms/android/platform_www/cordova_plugins.js
+++ ebekala/platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,22 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "onesignal-cordova-plugin.OneSignal",
+    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+    "pluginId": "onesignal-cordova-plugin",
+    "clobbers": [
+      "OneSignal"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open"
+    ]
+  },
+  {
     "id": "cordova-connectivity-monitor.connectivity",
     "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
     "pluginId": "cordova-connectivity-monitor",
@@ -16,26 +32,19 @@ module.exports = [
       "window.admob",
       "window.tappx"
     ]
-  },
-  {
-    "id": "onesignal-cordova-plugin.OneSignal",
-    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-    "pluginId": "onesignal-cordova-plugin",
-    "clobbers": [
-      "OneSignal"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-connectivity-monitor": "1.2.2",
-  "cordova-play-services-version-adapter": "1.0.2",
-  "cordova-admob": "5.1.0",
   "cordova-android-firebase-gradle-release": "4.0.0",
   "cordova-android-support-gradle-release": "3.0.1",
   "onesignal-cordova-plugin": "2.11.3",
-  "cordova-plugin-whitelist": "1.3.4"
+  "cordova-plugin-whitelist": "1.3.4",
+  "cordova-plugin-inappbrowser": "4.1.0",
+  "cordova-connectivity-monitor": "1.2.2",
+  "cordova-play-services-version-adapter": "1.0.2",
+  "cordova-admob": "5.1.0"
 };
 // BOTTOM OF METADATA
 });
