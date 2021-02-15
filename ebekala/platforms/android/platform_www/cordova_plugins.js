96b4c258305d07fb671bb5f9f232659682d91aff
walaa111 <walaagamil18@gmail.com>
2021-02-15 21:05:57 +0200

    edit

--- ebekala/platforms/android/platform_www/cordova_plugins.js
+++ ebekala/platforms/android/platform_www/cordova_plugins.js
@@ -1,30 +1,36 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-  {
-    "id": "cordova-connectivity-monitor.connectivity",
-    "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
-    "pluginId": "cordova-connectivity-monitor",
-    "clobbers": [
-      "window.connectivity"
-    ]
-  },
-  {
-    "id": "cordova-admob.AdMobAds",
-    "file": "plugins/cordova-admob/www/admob.js",
-    "pluginId": "cordova-admob",
-    "clobbers": [
-      "window.admob",
-      "window.tappx"
-    ]
-  }
-];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-  "cordova-connectivity-monitor": "1.2.2",
-  "cordova-play-services-version-adapter": "1.0.2",
-  "cordova-admob": "5.1.0",
-  "cordova-android-firebase-gradle-release": "4.0.0"
-};
-// BOTTOM OF METADATA
+  module.exports = [
+    {
+      "id": "cordova-connectivity-monitor.connectivity",
+      "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
+      "pluginId": "cordova-connectivity-monitor",
+      "clobbers": [
+        "window.connectivity"
+      ]
+    },
+    {
+      "id": "cordova-admob.AdMobAds",
+      "file": "plugins/cordova-admob/www/admob.js",
+      "pluginId": "cordova-admob",
+      "clobbers": [
+        "window.admob",
+        "window.tappx"
+      ]
+    },
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-connectivity-monitor": "1.2.2",
+    "cordova-play-services-version-adapter": "1.0.2",
+    "cordova-admob": "5.1.0",
+    "cordova-plugin-inappbrowser": "4.1.0",
+    "cordova-plugin-whitelist": "1.3.4"
+  };
 });
